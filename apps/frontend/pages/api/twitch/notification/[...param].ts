27bcd9b6d69eefdcfbb7b4794f8ad7bebdd4cbc1
alexweininger <alex.weininger@live.com>
2021-10-10 22:51:26 -0700

    Get raw body

--- apps/frontend/pages/api/twitch/notification/[...param].ts
+++ apps/frontend/pages/api/twitch/notification/[...param].ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors';
-import axios from 'axios';
 import crypto from 'crypto';
 
 type VerificationBody = {
@@ -11,6 +10,12 @@ type VerificationBody = {
 enum MessageType {
     Notification = 'notification',
     Verification = 'webhook_callback_verification'
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -27,10 +32,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const param = req.query.param as string[];
     console.log(`POST - Notification: ${param.join(', ')}`)
 
+    const rawBody = await webhookPayloadParser(req);
+
     const messageSignature = req.headers["Twitch-Eventsub-Message-Signature"] as string;
     const messageId = req.headers["Twitch-Eventsub-Message-Id"] as string;
     const messageTimestamp = req.headers["Twitch-Eventsub-Message-Timestamp"] as string;
+
     if (!verifySignature(messageSignature, messageId, messageTimestamp, req.body)) {
+        console.log('Request verification failed.');
         res.status(403).send("Forbidden"); // Reject requests with invalid signatures
         res.end();
     }
@@ -38,13 +47,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const messageType: MessageType = req.headers['Twitch-Eventsub-Message-Type'] as MessageType;
 
     if (messageType === MessageType.Verification) {
+        console.log('verifying webhook...');
         const challenge: string = (req.body as VerificationBody).challenge;
+        console.log('challenge: ', challenge);
         res.send(challenge);
         res.end();
     }
 
     if (messageType === MessageType.Notification) {
-        console.log(req.body.event);
+        const jsonBody = JSON.parse(rawBody);
+        console.log(jsonBody.event);
         res.status(200);
         res.end();
     }
@@ -56,3 +68,14 @@ function verifySignature(messageSignature: string, id: string, timestamp: string
     const expectedSignatureHeader = "sha256=" + signature.digest("hex");
     return expectedSignatureHeader === messageSignature;
 }
+
+const webhookPayloadParser: (req: NextApiRequest) => Promise<string> = (req: NextApiRequest) =>
+    new Promise((resolve) => {
+        let data = "";
+        req.on("data", (chunk) => {
+            data += chunk;
+        });
+        req.on("end", () => {
+            resolve(Buffer.from(data).toString());
+        });
+    });
