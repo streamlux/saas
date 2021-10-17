a86b7a560aecff1740037296883d1d382596159b
alexweininger <alex.weininger@live.com>
2021-10-17 03:19:25 -0700

    Hack to ensure @emotion/react is included in package.json

--- apps/frontend/definitions/chakra/theme/styles.ts
+++ apps/frontend/definitions/chakra/theme/styles.ts
@@ -1,4 +1,7 @@
 import { ThemeOverride } from "@chakra-ui/react";
+// Do not remove below import, it's needed to make sure @emotion/react is included in the generated package.json file when we build our app for production.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Global } from "@emotion/react";
 
 type GlobalStyles = Pick<ThemeOverride, "styles">;
 
