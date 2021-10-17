124111f5788fab7afa427422a35ef161e7609e33
alexweininger <alex.weininger@live.com>
2021-10-17 04:12:47 -0700

    Add franer-motion and @emotion/styled

--- apps/frontend/definitions/chakra/theme/styles.ts
+++ apps/frontend/definitions/chakra/theme/styles.ts
@@ -1,7 +1,13 @@
 import { ThemeOverride } from "@chakra-ui/react";
-// Do not remove below import, it's needed to make sure @emotion/react is included in the generated package.json file when we build our app for production.
+
+// Do not remove below imports, it's needed to make sure @emotion/react is included in the generated package.json file when we build our app for production.
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Global } from "@emotion/react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { StyledComponent } from "@emotion/styled";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AnimationOptions } from "framer-motion";
 
 type GlobalStyles = Pick<ThemeOverride, "styles">;
 
